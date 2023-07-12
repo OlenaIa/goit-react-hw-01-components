d3eed3aba377af8a144a5b216b293400fc3ac134
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-07-12 16:21:31 +0300

    task 1 + readme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import user from "./user.json";
+import { Profile } from './Profile/Profile'
+
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        display: 'block',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <div>React homework template</div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,41 @@
+import css from './Profile.module.css'
+import PropTypes from "prop-types";
+
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+    return (<div className={css.profile}>
+        <div className={css.description}>
+            <img
+                src={avatar}
+                alt={username}
+                className={css.avatar}
+            />
+            <p className={css.name}>{username}</p>
+            <p className="tag">{tag}</p>
+            <p className="location">{location}</p>
+        </div>
+
+        <ul className={css.stats}>
+            <li>
+                <span className={css.label}>Followers</span>
+                <span className={css.quantity}>{stats.followers}</span>
+            </li>
+            <li>
+                <span className={css.label}>Views</span>
+                <span className={css.quantity}>{stats.views}</span>
+            </li>
+            <li>
+                <span className={css.label}>Likes</span>
+                <span className={css.quantity}>{stats.likes}</span>
+            </li>
+        </ul>
+    </div>);
+};
+
+Profile.propTypes = {
+    username: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    avatar: PropTypes.string,
+    stats: PropTypes.objectOf(PropTypes.number),
+};
