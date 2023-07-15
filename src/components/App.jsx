c3d9c1e2517f145bf1f6671e786ebff59613331d
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-07-15 14:49:56 +0300

    add data folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from "./user.json";
-import data from './data.json';
-import friends from './friends.json';
-import transactions from "./transactions.json";
+import user from "../data/user.json";
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from "../data/transactions.json";
 import { Profile } from './Profile/Profile'
 import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
